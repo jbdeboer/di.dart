b0f858597f823ef9d38dee8f8dd04ce6ef5690a8
James deBoer <james@huronbox.com>
2014-04-11 16:35:57 -0700

    injector array

--- lib/key.dart
+++ lib/key.dart
@@ -1,12 +1,17 @@
 part of di;
 
+int _uniqKey = 0;
+Map<int, int> hashToKey = {};
+
 class Key {
   final Type type;
   final Type annotation;
   int hashCode;
+  int key;
 
   Key(this.type, [this.annotation]) {
     hashCode = type.hashCode + annotation.hashCode;
+    key = hashToKey.putIfAbsent(hashCode, () => _uniqKey++);
   }
 
   bool operator ==(other) =>
